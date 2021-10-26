763f86f9f7812cf817140969358370f9747fcda8
Lars Vilhuber <lars@vilhuber.com>
2021-10-26 12:40:50 -0400

    Adjusting to our needs

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,17 +1,9 @@
 #!/bin/bash
 
-# install stata
-mkdir ~/Downloads
-cd ~/Downloads
-wget -O Stata$2Linux64.tar.gz $1
-cd /tmp/
-mkdir statafiles
-cd statafiles
-tar -zxf ~/Downloads/Stata$2Linux64.tar.gz
-cd /usr/local
-mkdir stata$2
-cd stata$2
-yes | /tmp/statafiles/install
+cwd=$(pwd)
+
+# initialize license
+cd /usr/local/stata$2
 export PATH=/usr/local/stata$2:$PATH
 ./stinit << EOF
 y
@@ -27,26 +19,18 @@ y
 EOF
 
 # back to the default working directory
-cd /github/workspace
+cd $cwd
+file=$1
+basefile=${file%*.do}
 
 # run do-file
-echo $8
-if [ $8 == "stata" ]; then
-    echo $8
-    stata -b do main
-elif [ $8 == "stata-se" ]; then
-    echo $8
-    stata-se -b do main
-elif [ $8 == "stata-mp" ]; then
-    echo $8
-    stata-mp -b do main
-fi
+stata-mp -b do $basefile
 
 # print log result
-cat main.log
+cat ${basefile}.log
 
 # Fail CI if Stata ran with an error
-EXIT_CODE=$(tail -1 main.log | tr -d '[:cntrl:]')
+EXIT_CODE=$(tail -1 ${basefile}.log | tr -d '[:cntrl:]')
 
 if [[ ${EXIT_CODE:0:1} == "r" ]]; then
     exit 1
